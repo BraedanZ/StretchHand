e33a122c742cb8debb8150e0719042e10bf4a1d4
BraedanZ <60045806+BraedanZ@users.noreply.github.com>
2024-03-28 17:38:29 -0400

    Begin setting up hands

--- StretchHand/Assets/Scripts/PlayerController.cs
+++ StretchHand/Assets/Scripts/PlayerController.cs
@@ -4,21 +4,40 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    Vector3 mousePos;
+
+    Hand left;
+    Hand right;
+
     void Start()
     {
-        
+       left = GameObject.FindGameObjectWithTag("Left").GetComponent<Hand>(); 
+       right = GameObject.FindGameObjectWithTag("Right").GetComponent<Hand>(); 
     }
 
     void Update()
     {
+        UpdateMousePosition();
         DetectMouseInput();
     }
 
+    private void UpdateMousePosition() {
+        mousePos = Input.mousePosition;
+        Debug.Log(mousePos);
+    }
+
     private void DetectMouseInput() {
+
         if (Input.GetMouseButtonDown(0)) {
-            Debug.Log("Pressed left click");
-        } else if (Input.GetMouseButtonDown(1)) {
-            Debug.Log("Pressed right click");
+            LaunchHand(left);
+        } 
+        
+        if (Input.GetMouseButtonDown(1)) {
+            LaunchHand(right);
         }
+    }
+
+    private void LaunchHand(Hand hand) {
+
     }
 }
